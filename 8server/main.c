c0b85551364c4146ab543b04ec7680eb2ce57dfd
Gyan deep <gnani258@gmail.com>
2012-10-10 09:33:10 +0530

    Initial Commit

---
+++ 8server/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  8server
+//
+//  Created by Gyan deep on 10/10/12.
+//  Copyright (c) 2012 Gyan deep. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
